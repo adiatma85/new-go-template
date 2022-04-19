d25eb947dd2131afce9f26e75ef437846f5d4eaa
adiatma85 <adiatma85@gmail.com>
2022-04-19 12:56:27 +0700

    add: package and reference for projects

--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
+	// Reference for this project --> https://intersog.com/blog/how-to-write-a-custom-url-shortener-using-golang-and-redis/
 	fmt.Println("Hello World")
 }
