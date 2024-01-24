c549f91a008bed8889c48ac0baca5b2fb87301ea
Koernia <adiatma85@gmail.com>
2024-01-24 19:18:24 +0700

    major changes for template

--- docs/sql/000_schema.sql
+++ docs/sql/000_schema.sql
@@ -1,4 +1,4 @@
--- Create Table Users
+-- [DDL] Create Table Users
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -19,23 +19,35 @@ CREATE TABLE IF NOT EXISTS `user` (
   UNIQUE (`username`)
 ) ENGINE = INNODB COMMENT='User table';
 
--- Create Table Urls
--- For example from url shortener
-DROP TABLE IF EXISTS `url`;
-CREATE TABLE IF NOT EXISTS `url` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `fk_user_id` INT COMMENT 'Foreign Key to User ID',
-  `original_url` VARCHAR(255) NOT NULL DEFAULT '',
-  `shorten_url` VARCHAR(255) NOT NULL DEFAULT '',
-  `visit` INT NOT NULL DEFAULT '0',
+-- [DDL] Create new table for Role
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE IF NOT EXISTS `role` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL DEFAULT '',
+    `type` VARCHAR(255) NOT NULL DEFAULT '',
+    `rank` INT NOT NULL DEFAULT 0,
 
-  -- Utility columns
-  `status` SMALLINT NOT NULL DEFAULT '1',
-  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `created_by` VARCHAR(255),
-  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_by` VARCHAR(255),
-  `deleted_at`TIMESTAMP,
-  `deleted_by` VARCHAR(255),
-  PRIMARY KEY (`id`)
-) ENGINE = INNODB COMMENT='url table';
+    -- Utility columns
+    `status` SMALLINT NOT NULL DEFAULT '1',
+    `flag` INT NOT NULL DEFAULT '0',
+    `meta` VARCHAR(255),
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `created_by` VARCHAR(255),
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `updated_by` VARCHAR(255),
+    `deleted_at`TIMESTAMP,
+    `deleted_by` VARCHAR(255),
+    PRIMARY KEY (`id`)
+);
+
+
+-- [DML] Insert Dummy Data for User and for Category Tables
+INSERT INTO `user` (`email`, `username`, `password`, `display_name`)
+VALUES 
+('adiatma85@gmail.com', 'adiatma85', '$2a$10$hEU84tig1.W0TcoSe5.zwushMbDarsTnaXadC5/Y/difKiatAHuGO', 'Luki');
+
+-- [DML] Populate role admin and user
+INSERT INTO `role` (`name`, `type`, `rank`) VALUES
+('Super Admin', 'admin', 1),
+('User', 'user', 2)
+;
--- src/handler/user.go
+++ src/handler/user.go
@@ -6,8 +6,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// @Summary Get User List
-// @Description Get list all user
+// @Summary Get User List as an Admin
+// @Description Get list all user Get User List as an Admin
 // @Security BearerAuth
 // @Tags Admin
 // @Param limit query integer false "limit"
@@ -17,7 +17,7 @@ import (
 // @Success 200 {object} entity.HTTPResp{data=[]entity.User{}}
 // @Failure 500 {object} entity.HTTPResp{}
 // @Router /v1/admin/user [GET]
-func (r *rest) GetListUser(ctx *gin.Context) {
+func (r *rest) GetListUserAsAdmin(ctx *gin.Context) {
 	var param entity.UserParam
 	if err := r.BindParams(ctx, &param); err != nil {
 		r.httpRespError(ctx, err)
@@ -61,13 +61,13 @@ func (r *rest) GetUserByID(ctx *gin.Context) {
 // @Summary Update One User
 // @Description Update one user detail
 // @Security BearerAuth
-// @Tags User
+// @Tags Admin
 // @Param user_id path integer true "user id"
 // @Param user body entity.UpdateUserParam true "user data"
 // @Produce json
 // @Success 200 {object} entity.HTTPResp{data=entity.User{}}
 // @Failure 500 {object} entity.HTTPResp{}
-// @Router /v1/user/{user_id} [PUT]
+// @Router /v1/admin/user/{user_id} [PUT]
 func (r *rest) UpdateUser(ctx *gin.Context) {
 	var updateParam entity.UpdateUserParam
 
@@ -93,12 +93,12 @@ func (r *rest) UpdateUser(ctx *gin.Context) {
 // @Summary Delete User
 // @Description Soft delete user data
 // @Security BearerAuth
-// @Tags User
+// @Tags Admin
 // @Param user_id path integer true "user id"
 // @Produce json
 // @Success 200 {object} entity.HTTPResp{data=entity.User{}}
 // @Failure 500 {object} entity.HTTPResp{}
-// @Router /v1/user/{user_id} [DELETE]
+// @Router /v1/admin/user/{user_id} [DELETE]
 func (r *rest) DeleteUser(ctx *gin.Context) {
 	var selectParam entity.UserParam
 
@@ -108,6 +108,67 @@ func (r *rest) DeleteUser(ctx *gin.Context) {
 	}
 
 	if err := r.uc.User.Delete(ctx.Request.Context(), selectParam); err != nil {
+		r.httpRespError(ctx, err)
+		return
+	}
+
+	r.httpRespSuccess(ctx, codes.CodeSuccess, nil, nil)
+}
+
+// @Summary Get User Self Profile
+// @Description Get user details self profile
+// @Security BearerAuth
+// @Tags User
+// @Produce json
+// @Success 200 {object} entity.HTTPResp{data=entity.User{}}
+// @Failure 500 {object} entity.HTTPResp{}
+// @Router /v1/user/profile [GET]
+func (r *rest) UserProfile(ctx *gin.Context) {
+	userProfile, err := r.uc.User.GetSelfProfile(ctx.Request.Context())
+	if err != nil {
+		r.httpRespError(ctx, err)
+		return
+	}
+
+	r.httpRespSuccess(ctx, codes.CodeSuccess, userProfile, nil)
+}
+
+// @Summary Self Delete for User
+// @Description Self Delete for User
+// @Security BearerAuth
+// @Tags User
+// @Produce json
+// @Success 200 {object} entity.HTTPResp{}
+// @Failure 500 {object} entity.HTTPResp{}
+// @Router /v1/user/profile/self-delete [DELETE]
+func (r *rest) UserSelfDelete(ctx *gin.Context) {
+	err := r.uc.User.SelfDelete(ctx.Request.Context())
+	if err != nil {
+		r.httpRespError(ctx, err)
+		return
+	}
+
+	r.httpRespSuccess(ctx, codes.CodeSuccess, nil, nil)
+}
+
+// @Summary Change password profile for User
+// @Description Change Password for User
+// @Security BearerAuth
+// @Tags User
+// @Produce json
+// @Success 200 {object} entity.HTTPResp{}
+// @Failure 500 {object} entity.HTTPResp{}
+// @Router /v1/user/profile/change-password [PUT]
+func (r *rest) UserChangePassword(ctx *gin.Context) {
+	var updateParam entity.ChangePasswordRequest
+
+	if err := r.Bind(ctx, &updateParam); err != nil {
+		r.httpRespError(ctx, err)
+		return
+	}
+
+	err := r.uc.User.ChangePassword(ctx.Request.Context(), updateParam)
+	if err != nil {
 		r.httpRespError(ctx, err)
 		return
 	}
